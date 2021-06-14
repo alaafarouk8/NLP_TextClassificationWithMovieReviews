57b5cc520e9f6cd7b5d363b1d83331014f0aeba5
ALaa Farouk <alaafar19@gmail.com>
2021-06-14 06:21:07 +0200

    getmeanvector

--- Assignment3.py
+++ Assignment3.py
@@ -11,6 +11,7 @@ import nltk
 import sklearn
 import numpy as np
 import numpy
+import pandas as pd
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer 
 from nltk.stem import WordNetLemmatizer 
@@ -18,19 +19,27 @@ from sklearn.metrics import plot_roc_curve
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+
 import gensim
-from gensim.models import Word2vec
+from gensim.models import Word2Vec
 ######################################################################################
 # Init the Wordnet Lemmatizer
 lemmatizer = WordNetLemmatizer()
 stemmer = WordNetLemmatizer()
+# Set values for various parameters
+feature_size = 10    # Word vector dimensionality  
+window_context = 10          # Context window size                                                                                    
+min_word_count = 1   # Minimum word count                        
+sample = 1e-3   # Downsample setting for frequent words
 files = []
+######################################################################################
 # load files is done
 def LoadFiles():
-    positive , negative = 0 , 0
     dataset = sklearn.datasets.load_files(r"C:\Users\ALAA\OneDrive\Documents\GitHub\NLP_TextClassificationWithMovieReviews\dataset", description=None, categories=None, load_content=True, shuffle=True, random_state=0)
     X , Y = dataset.data , dataset.target     # y is an array of 0s , 1s.
     return X , Y 
+######################################################################################
 # preparing data 
 def  Preparedata(X):
     for i in range(len(X)):
@@ -45,21 +54,34 @@ def  Preparedata(X):
          file = ' '.join(file)
          files.append(file)
     return files
+
+#######################################################################################
+#Averaging Words Vectors to Create Sentence Embedding
+def get_mean_vector(word2vec_model, words):
+    vocabulary = word2vec_model.wv.key_to_index
+    words = [(word) for word in words if word in vocabulary]
+    if len(words) >= 1:
+        return np.mean(word2vec_model.wv[words], axis=0)
+    else:
+        return np.zeros(word2vec_model.vector_size)
+######################################################################################
+# divide the data into 20% test set and 80% training set.
+def Splitingthedata(x,y):
+    Xtrain, Xtest, Ytrain, Ytest = train_test_split(x, y, test_size=0.2, random_state=0)
+    return Xtrain, Xtest, Ytrain, Ytest
+
 x,y = LoadFiles()
 files_=Preparedata(x)
-print(len(files))
-# Set values for various parameters of word2vec
-num_features = 250  # Word vector dimensionality. Determines the no of words each word in the vocabulary will
-#be associated with. Must be tuned.
-min_word_count = 50   # Minimum word count. Wods occuring below the threshold will be ignored
-num_workers = 1       # Number of threads to run in parallel
-context = 15          # Context window size to be considered for each word                                             
-downsampling = 1e-3   # Downsample setting for frequent words. To prevent more frequent words from dominating.
-
-# Create Skip Gram model
-model1= gensim.models.Word2Vec(files, min_count = 1,
-                                             window = 5,)
-# Print results
-print("Cosine similarity between 'actor' " + 
-               "and 'good' - CBOW : ",
-    model1.similar_by_word('actor', 'good'))
+xtrain , xtest , ytrain , ytest = Splitingthedata(files_,y)
+model =gensim.models.Word2Vec(files_, min_count=2,sample=sample)
+train_array=[]
+test_array=[]
+for i in xtrain:
+    vec = get_mean_vector(model, i)
+    if len(vec) > 0:
+        train_array.append(vec)
+        
+for i in xtest:
+    vec = get_mean_vector(model, i)
+    if len(vec) > 0:
+        test_array.append(vec)
